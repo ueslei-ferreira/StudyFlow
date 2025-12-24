b4ae90c7ee0755a64d366944255ad347a7b6aa61
Ueslei GIT <ueslei392@gmail.com>
2025-12-24 14:37:23 -0300

    prepara para deploy

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api/v1',
+  baseURL: (import.meta.env.VITE_API_BASE_URL || '') + '/api/v1',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 api.interceptors.request.use((config) => {
